e8820260c79b3e39016be678d31893ec0b39552f
Even Rouault <even.rouault@spatialys.com>
2024-10-20 20:32:52 +0200

    Add colors

--- relationships.py
+++ relationships.py
@@ -20,13 +20,31 @@ python = grouped_nodes.get('Python', [])
 java = grouped_nodes.get('Java', [])
 R = grouped_nodes.get('R', [])
 
+color_map = {
+    'C_CPP': 'lightblue',
+    'Python': 'lightgreen',
+    'Java': 'salmon',
+    'R': 'orange'
+}
+
+node_colors = []
+for node in graph.nodes():
+    group = df.loc[df['target'] == node, 'target_nature'].values
+    if len(group) > 0:
+        node_colors.append(color_map.get(group[0], 'gray'))
+    elif node in center_nodes:
+        node_colors.append("lightblue")
+    else:
+        print(node)
+        assert False
+
 shells = [center_nodes, c_cpp + python + java + R]
 
 layout = nx.shell_layout(graph, nlist=shells)
 
 nx.draw(graph, layout,
         edge_color='gray',
-        node_color='lightblue',
+        node_color=node_colors,
         arrowsize=20,
         with_labels=True)
 
