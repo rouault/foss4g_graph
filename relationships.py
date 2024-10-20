3a4e5768efe5bc145c218b8ed0b69590ac7d57f8
Even Rouault <even.rouault@spatialys.com>
2024-10-20 21:54:27 +0200

    With JTS

--- relationships.py
+++ relationships.py
@@ -11,7 +11,7 @@ graph = nx.from_pandas_edgelist(df, source='source', target='target',
 
 plt.figure(figsize=(10, 10))
 
-center_nodes = ["GDAL", "GEOS", "PROJ"]
+center_nodes = ["JTS", "GDAL", "GEOS", "PROJ"]
 
 grouped_nodes = df.groupby('target_nature')['target'].apply(list).to_dict()
 
@@ -32,8 +32,10 @@ for node in graph.nodes():
     group = df.loc[df['target'] == node, 'target_nature'].values
     if len(group) > 0:
         node_colors.append(color_map.get(group[0], 'gray'))
+    elif node == "JTS":
+        node_colors.append(color_map["Java"])
     elif node in center_nodes:
-        node_colors.append("lightblue")
+        node_colors.append(color_map["C_CPP"])
     else:
         print(node)
         assert False
@@ -42,11 +44,29 @@ shells = [center_nodes, c_cpp + python + java + R]
 
 layout = nx.shell_layout(graph, nlist=shells)
 
-nx.draw(graph, layout,
-        edge_color='gray',
-        node_color=node_colors,
-        arrowsize=20,
-        with_labels=True)
+if False:
+    nx.draw(graph, layout,
+            edge_color='gray',
+            node_color=node_colors,
+            arrowsize=20,
+            with_labels=True)
+nx.draw_networkx_nodes(graph, layout, node_size=500, node_color=node_colors)
+nx.draw_networkx_labels(graph, layout, font_size=10, font_weight='bold')
+
+edge_styles = []
+for _, row in df.iterrows():
+    if row['dep_nature'] == 'direct':
+        edge_styles.append('solid')  # Continuous line
+    elif row['dep_nature'] == 'port':
+        edge_styles.append('dashed')  # Dashed line
+    else:
+        print(row)
+        assert False
+
+for i, (source, target) in enumerate(graph.edges()):
+    style = edge_styles[i]
+    nx.draw_networkx_edges(graph, layout, edgelist=[(source, target)], style=style, width=1.5, edge_color='gray')
+    #plt.plot(*zip(layout[source], layout[target]), linestyle=style, color='gray')
 
 plt.title('Dependencies between FOSS4G stack')
 plt.savefig('relationships.png', format='png', dpi=300)
